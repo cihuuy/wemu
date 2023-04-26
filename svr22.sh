9eea7270919720c81a93e728289036aa6df149ea
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-04-26 14:04:00 +0700

    Update svr22.sh

--- svr22.sh
+++ svr22.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # Make Instance Ready for Remote Desktop or RDP
 apt-get update
+rm -rf windows2022.img windows2022.gz ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "Download windows files"
-wget -O win7lite.img.gz https://archive.org/download/w7-qcow2/win7lite.img.gz
-gunzip win7lite.img.gz
+wget -O windows2022.gz https://archive.org/download/windows2022/windows2022.gz
+gunzip windows2022.gz
 echo "Wait..."
 echo "I m Working Now.."
-mv win7lite win7lite.img
 wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
@@ -28,7 +28,7 @@ echo Downloading files from aank.me
 apt-get install qemu-system-x86 -y
 echo "Wait..."
 echo "Starting Windows"
-qemu-system-x86_64 -hda win7lite.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda windows2022.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
@@ -41,7 +41,7 @@ echo "Don't closse this Tab"
 echo "Wait 1 - 3 minut for finishing bot"
 echo "RDP run up to 50 hours"
 echo "Support YT Channel-> Aank is ME, thankyou"
-echo "Link-> wanpeace"
+echo "Link-> https://aank.me/Youtube"
 echo "===================================="
 b='\033[1m'
 r='\E[31m'
