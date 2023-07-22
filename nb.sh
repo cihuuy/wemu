5b610a1c00129904fd492ab8818b4c138ed3bf7c
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-07-22 10:16:17 +0700

    Update nb.sh

--- nb.sh
+++ nb.sh
@@ -14,10 +14,7 @@ function goto
 
 : ngrok
 
-echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok authtoken $CRP --config /storage/ssd5/968/17520968/public_html/1/dg/ngrok.yml
-./ngrok tcp 4000 --config /storage/ssd5/968/17520968/public_html/1/dg/ngrok.yml &>/dev/null &
+./ngrok tcp 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
@@ -25,7 +22,7 @@ clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
 echo IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+curl --silent --show-error http://127.0.0.1:5000/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
 echo User: user
 echo Passwd: 123456
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
