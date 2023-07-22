e44aa1ef6372adbeffe0c125cfe6ca4cdcaa257e
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-07-22 10:25:50 +0700

    Update nb.sh

--- nb.sh
+++ nb.sh
@@ -14,7 +14,6 @@ function goto
 
 : ngrok
 
-./ngrok tcp 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:5000/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
